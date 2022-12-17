c333153fdde7f30f42cc2ccc39be0bc9f044b3f0
Sami Tuomainen <satuomainen@gmail.com>
2022-12-17 18:55:38 +0200

    App skeleton from Spring Initializr

---
+++ src/main/java/org/havis/meterpost/MeterpostApplication.java
@@ -0,0 +1,13 @@
+package org.havis.meterpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeterpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeterpostApplication.class, args);
+	}
+
+}
